419d9e4d0b5395d4fba264bf0620bc00ea17ec78
Robert-Song <thinkmisul@gmail.com>
2024-10-19 01:00:00 -0400

    constructor and move_valid function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { drawCheckerboard } from "./draw.js";
+import { Board } from "./board.js";
+
 const content_div = document.getElementById("screen") as HTMLCanvasElement;
 const row_input = document.getElementById("x_c") as HTMLInputElement;
 const column_input = document.getElementById("y_c") as HTMLInputElement;
 const start_button = document.getElementById("start_button") as HTMLButtonElement;
 start_button.addEventListener('click', () => { drawCheckerboard(content_div, row_input.value as unknown as number, parseInt(column_input.value)); });
 content_div.addEventListener('click', (e) => { console.log(e.offsetX); })
+var board = new Board(3, 3); //testing, making 3x3 board
