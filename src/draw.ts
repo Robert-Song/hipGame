5911dbfff8231605ec5c5cd68f25fe195a6f19ee
RMRattray <rmr3141@gmail.com>
2024-10-20 09:21:17 -0400

    Hovering now works

--- src/draw.ts
+++ src/draw.ts
@@ -1,46 +1,92 @@
 import { Board } from './board.js';
 
-// Define a function to adjust screen proportions and draw a checkerboard
-export function drawCheckerboard(screen: HTMLCanvasElement, m: number, n: number) {
-    // Calculate the new dimensions while preserving the area
-    const currentArea = screen.width * screen.height;
-    const checkerDim = Math.sqrt(currentArea / (m * n));
+export class drawManager {
+    screen: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    board: Board;
+    cdim: number;
+    hoveri: number;
+    hoverj: number;
+    constructor(screen: HTMLCanvasElement, board: Board) {
+        this.screen = screen;
+        this.ctx = this.screen.getContext("2d");
+        this.board = board
+        this.cdim = Math.sqrt(screen.width * screen.height / (board.m * board.n));
+        this.hoveri = -1;
+        this.hoverj = -1;
+    }
 
-    // Adjust the screen dimensions
-    screen.width = checkerDim * m;
-    screen.height = checkerDim * n;
+    updateDims(m: number, n: number) {
+        // When dimensions change, update the board
+        // and the screen
+        this.board = new Board(m, n);
+        this.cdim = Math.sqrt(this.screen.width * this.screen.height / (this.board.m * this.board.n));
+        this.screen.width = this.cdim * m;
+        this.screen.height = this.cdim * n;
+    }
 
-
-    // Draw the checkerboard
-    const ctx = screen.getContext("2d")
-
-    // fillRect(x, y, width, height)
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            ctx.fillStyle = (i + j) % 2 ? "red" : "blue";
-            ctx.fillRect(i*checkerDim, j*checkerDim, checkerDim, checkerDim)
+    drawCheckerboard() {
+        for (let i = 0; i < this.board.m; i++) {
+            for (let j = 0; j < this.board.n; j++) {
+                this.ctx.fillStyle = (i + j) % 2 ? "lightGreen" : "lightBlue";
+                this.ctx.fillRect(i*this.cdim, j*this.cdim, this.cdim, this.cdim)
+            }
         }
     }
 
-}
-
-// Is the canvas's click event handler
-export function getClickOnCheckerboard(the_board: Board, x: number, y: number, screen: HTMLCanvasElement) {
-    // checks if valid move
-    const c = screen.height / the_board.n;
-    const i = Math.trunc(x / c);
-    const j = Math.trunc(y / c);
-    if (the_board.is_empty(i, j)) {
-        // if so, make move
-        the_board.move(i, j, true)
-        // and draw a marker there!
-        const ctx = screen.getContext("2d");
-        ctx.fillStyle = '#550000';
+    drawTentativeMarker(i: number, j: number) {
         const myPath = new Path2D();
-        myPath.moveTo((i + 0.8)*c, (j + 0.5)*c);
-        myPath.arc((i + 0.5)*c, (j + 0.5)*c, 0.3*c, 0, 2 * Math.PI);
-        ctx.fill(myPath);
-        return true;
+        myPath.moveTo((i + 0.75)*this.cdim, (j + 0.5)*this.cdim);
+        myPath.arc((i + 0.5)*this.cdim, (j + 0.5)*this.cdim, 0.25*this.cdim, 0, 2 * Math.PI);
+        this.ctx.fillStyle = "green"
+        this.ctx.fill(myPath);
     }
-    return false;
+
+    coverTentativeMarker(i: number, j: number) {
+        if (i < 0 || j < 0) return;
+        this.ctx.fillStyle = (i + j) % 2 ? "lightGreen" : "lightBlue";
+        this.ctx.fillRect(i*this.cdim, j*this.cdim, this.cdim, this.cdim)
+    }
+
+    drawMarker(i: number, j: number, isHumanPlayer: boolean) {
+        const myPath = new Path2D();
+        myPath.moveTo((i + 0.8)*this.cdim, (j + 0.5)*this.cdim);
+        myPath.arc((i + 0.5)*this.cdim, (j + 0.5)*this.cdim, 0.3*this.cdim, 0, 2 * Math.PI);
+        this.ctx.fillStyle = isHumanPlayer ? "darkgreen" : "darkblue"
+        this.ctx.fill(myPath);
+    }
+
+    updateHover(x: number, y: number) {
+        // When hovering - find checker over which being hovered
+        const i = Math.trunc(x / this.cdim);
+        const j = Math.trunc(y / this.cdim);
+        if (!(i == this.hoveri && j == this.hoverj)) { // If it's changed,
+            if (this.hoveri >= 0 && this.board.is_empty(this.hoveri, this.hoverj)) {
+                this.coverTentativeMarker(this.hoveri, this.hoverj); // hide old hover (if applicable)
+            }
+            if (this.board.is_empty(i, j)) { // and if this one is an empty square,
+                this.drawTentativeMarker(i, j); // tentatively mark it
+                this.hoveri = i;
+                this.hoverj = j;
+            }
+        }
+    }
+
+    endHover() {
+        this.coverTentativeMarker(this.hoveri, this.hoverj);
+        this.hoveri = -1;
+        this.hoverj = -1;
+    }
+
+    // Is the canvas's click event handler
+    getClick(x: number, y: number) {
+        const i = Math.trunc(x / this.cdim);
+        const j = Math.trunc(y / this.cdim);
+        if (this.board.is_empty(i, j)) {
+            this.board.move(i, j, true)
+            this.drawMarker(i, j, true);
+            return true;
+        }
+        return false;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import { drawCheckerboard, getClickOnCheckerboard } from "./draw.js";
+import { drawManager } from "./draw.js";
 import { Board } from "./board.js"
 
+// Obtain relevant elements
 const content_div = document.getElementById("screen") as HTMLCanvasElement;
 const row_input = document.getElementById("x_c") as HTMLInputElement;
 const column_input = document.getElementById("y_c") as HTMLInputElement;
 const start_button = document.getElementById("start_button") as HTMLButtonElement;
-const the_board = new Board(parseInt(row_input.value), parseInt(column_input.value));
-start_button.addEventListener('click', () => { drawCheckerboard(content_div, row_input.value as unknown as number, parseInt(column_input.value)); });
-content_div.addEventListener('click', (e) => { getClickOnCheckerboard(the_board, e.offsetX, e.offsetY, content_div); })
+
+// Create MANAGER object
+const draw_manager = new drawManager(content_div, new Board(parseInt(row_input.value), parseInt(column_input.value)));
+draw_manager.drawCheckerboard();
+
+// Starting a new game updates dimensions and board
+start_button.addEventListener('click', () => { 
+    draw_manager.updateDims(parseInt(row_input.value), parseInt(column_input.value));
+    draw_manager.drawCheckerboard();
+});
+
+// Hovering over the canvas makes tentative markers appear
+content_div.addEventListener('mousemove', (e) => draw_manager.updateHover(e.offsetX, e.offsetY));
+content_div.addEventListener('mouseleave', () => draw_manager.endHover());
+
+// Clicking makes a move
+content_div.addEventListener('click', (e) => draw_manager.getClick(e.offsetX, e.offsetY));
