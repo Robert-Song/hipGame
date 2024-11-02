320548ac75a0fc161184b09fd564f9897bffd2e2
RMRattray <rmr3141@gmail.com>
2024-11-01 22:44:48 -0400

    Got AI turn indicator working

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ import { Board } from "./board.js"
 
 // Obtain relevant elements
 const content_div = document.getElementById("screen") as HTMLCanvasElement;
-const row_input = document.getElementById("x_c") as HTMLInputElement;
-const column_input = document.getElementById("y_c") as HTMLInputElement;
+const column_input = document.getElementById("x_c") as HTMLInputElement;
+const row_input = document.getElementById("y_c") as HTMLInputElement;
 const start_button = document.getElementById("start_button") as HTMLButtonElement;
+const plays_first_selection = document.getElementById("player") as HTMLSelectElement;
+const turn_indicator = document.getElementById("turner") as HTMLDivElement;
 
 // Create MANAGER object
-const draw_manager = new drawManager(content_div, new Board(parseInt(row_input.value), parseInt(column_input.value)));
+const draw_manager = new drawManager(content_div, turn_indicator, new Board(parseInt(row_input.value), parseInt(column_input.value)), (plays_first_selection.value == "0"));
 draw_manager.drawCheckerboard();
 
 // Starting a new game updates dimensions and board
 start_button.addEventListener('click', () => { 
-    draw_manager.updateDims(parseInt(row_input.value), parseInt(column_input.value));
+    draw_manager.updateDims(parseInt(column_input.value), parseInt(row_input.value), (plays_first_selection.value == "0"));
     draw_manager.drawCheckerboard();
 });
 
@@ -22,4 +24,6 @@ content_div.addEventListener('mousemove', (e) => draw_manager.updateHover(e.offs
 content_div.addEventListener('mouseleave', () => draw_manager.endHover());
 
 // Clicking makes a move
-content_div.addEventListener('click', (e) => draw_manager.getClick(e.offsetX, e.offsetY));
+content_div.addEventListener('click', (e) => {
+    draw_manager.getClick(e.offsetX, e.offsetY);
+} );
